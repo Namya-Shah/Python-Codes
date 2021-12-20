3c2b410e4d64b47ac18eb122d1f64e5d55148105
Namya Shah <neelshah@Namyas-MacBook-Air.local>
2021-12-20 10:52:50 +0530

    Defining a function

---
+++ basic-functions/defining_function.py
@@ -0,0 +1,11 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
+
+a = fizz_buzz(int(input("Enter number: ")))
