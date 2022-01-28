3319fd81a6242c73ed1893d1474a8748e0e3274a
Namya Shah <namya.shah123@gmail.com>
2022-01-28 11:29:44 +0530

    Added some more functionalities

--- mini-projects/notification_program.py
+++ mini-projects/notification_program.py
@@ -2,13 +2,10 @@ from socket import timeout
 import time
 from plyer import notification
 if __name__ == '__main__':
-    try:
-        while True:
-            notification.notify(
-                title = "Take a class",
-                message = "I have to take a python workshop",
-                timeout = 10
+    while True:
+        notification.notify(
+            title = "Take a class",
+            message = "I have to take a python workshop",
+            timeout = 10
             )
-            time.sleep(15)
-    except NotImplementedError:
-        print("You have no use of using notification method.")
+        time.sleep(15)
